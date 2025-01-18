89ffcfbb89abba2cb803b95c748d40ca2d825080
merliri-ma <realmagicethiopia@gmail.com>
2025-01-19 00:20:57 +0300

    Add files via upload

--- pages/api/planetary_hours.js
+++ pages/api/planetary_hours.js
@@ -1,6 +1,17 @@
-import { calculate_planetary_hours } from '../../api/planetary_hours.py'
 
 export default function handler(req, res) {
-  const planetaryData = calculate_planetary_hours()
-  res.status(200).json(planetaryData)
+  const now = new Date()
+  const planets = ['Sun', 'Moon', 'Mars', 'Mercury', 'Jupiter', 'Venus', 'Saturn']
+  
+  res.status(200).json({
+    planetary_hour: {
+      current_planet: planets[now.getHours() % planets.length],
+      power: Math.round(Math.random() * 100)
+    },
+    astronomical_data: {
+      ecliptic_longitude: Math.random() * 360,
+      right_ascension: Math.random() * 24,
+      declination: Math.random() * 180 - 90
+    }
+  })
 }
