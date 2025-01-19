04e9f6da1841d2345695d4b9d6258ca97e1e9a67
merliri-ma <realmagicethiopia@gmail.com>
2025-01-19 15:27:10 +0300

    Add files via upload

--- views/weekView.js
+++ views/weekView.js
@@ -1,19 +1,17 @@
 function displayWeekView(weekData) {
-    return `
-        <div class="week-view">
+    document.getElementById('result').innerHTML = `
+        <div class="week-display">
             <h2>Weekly Planetary Hours</h2>
             <div class="week-grid">
                 ${weekData.map(day => `
                     <div class="day-column">
-                        <h3>${day.date}</h3>
-                        <div class="hours-list">
-                            ${day.hours.map(hour => `
-                                <div class="hour-item">
-                                    <span>${hour.time}</span>
-                                    <span>${hour.planet}</span>
-                                </div>
-                            `).join('')}
-                        </div>
+                        <h3>${day.date.toLocaleDateString('en-US', {weekday: 'long'})}</h3>
+                        ${day.hours.map(hour => `
+                            <div class="hour-row">
+                                <span>Hour ${hour.number}</span>
+                                <span>${hour.planet}</span>
+                            </div>
+                        `).join('')}
                     </div>
                 `).join('')}
             </div>
