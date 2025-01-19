f472f9e9bd5cdf0b93c0192eaea7f89a73465681
merliri-ma <realmagicethiopia@gmail.com>
2025-01-19 10:34:15 +0300

    Delete components directory

--- components/PlanetModel.js
+++
@@ -1,8 +0,0 @@
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-
-export default function PlanetModel({ planetName }) {
-  const gltf = useLoader(GLTFLoader, `/3d/planets/${planetName}.glb`)
-  
-  return <primitive object={gltf.scene} />
-}
