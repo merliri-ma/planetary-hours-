5194538fa34e6a085afc0a07a98235458abfb625
merliri-ma <realmagicethiopia@gmail.com>
2025-01-19 15:26:40 +0300

    Add files via upload

--- views/hourView.js
+++ views/hourView.js
@@ -1,16 +1,12 @@
 function displayHourView(result) {
-    return `
-        <div class="hour-view">
+    document.getElementById('result').innerHTML = `
+        <div class="hour-display">
             <h2>Current Planetary Hour</h2>
-            <div class="current-hour-display">
-                <div class="time-info">
-                    <p>Current Time: ${result.time}</p>
-                    <p>Phase: ${result.isDay ? 'Day' : 'Night'}</p>
-                </div>
-                <div class="planet-info">
-                    <h3>${result.planet}</h3>
-                    <p>Hour Number: ${result.hourNumber}</p>
-                </div>
+            <div class="planet-info">
+                <h3>${result.planet}</h3>
+                <p>Time: ${new Date().toLocaleTimeString()}</p>
+                <p>Phase: ${result.isDay ? 'Day' : 'Night'}</p>
+                <p>Hour Number: ${result.hourNumber}</p>
             </div>
         </div>
     `;
